21fb9e1e50aa6e3b0906f70ae5eb4825d7391c38
chris20020109 <72123550+chris20020109@users.noreply.github.com>
2026-03-14 14:22:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,20 @@
 import streamlit as st
+from data.finmind_api import get_stock_price, get_eps
 
-st.title("股票分析系統")
+st.title("台股分析系統")
 
-stock = st.text_input("輸入股票代碼")
+stock = st.text_input("股票代碼", "2330")
 
-if stock:
-    st.write("股票代碼:", stock)
+if st.button("抓資料"):
+
+    price = get_stock_price(stock)
+
+    st.write("最近股價")
+
+    st.dataframe(price.tail())
+
+    eps = get_eps(stock)
+
+    st.write("EPS")
+
+    st.dataframe(eps.tail())
