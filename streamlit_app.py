8499ecfba42075c13cbe41c4f980aeb3b525101c
chris20020109 <72123550+chris20020109@users.noreply.github.com>
2026-03-14 14:12:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,8 @@
 import streamlit as st
 
-uploaded_files = st.file_uploader(
-    "Upload images", accept_multiple_files="directory", type=["jpg", "png", "jpeg"]
-)
-for uploaded_file in uploaded_files:
-    st.image(uploaded_file)
-st.subheader("操作")
-col1, col2, col3 = st.columns(3)
+st.title("股票分析系統")
 
-with col1:
-    st.button("儲存")
+stock = st.text_input("輸入股票代碼")
 
-with col2:
-    st.button("查看打卡紀錄")
-
-with col3:
-    st.button("生成/下載 NFT")
-    st.title("Web3打卡Demo")
-
-# 說明
-st.write("請上傳圖片，並按下儲存，系統會自動打卡（需登入 Metamask）。")
-
-# 檔案上傳
-uploaded_files = st.file_uploader(
-    "Upload images", accept_multiple_files="directory", type=["jpg", "png", "jpeg"]
-)
-for uploaded_file in uploaded_files:
-    st.image(uploaded_file)
-    
-# 按鈕
-st.subheader("操作")
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.button("儲存")
-
-with col2:
-    st.button("查看打卡紀錄")
-
-with col3:
-    st.button("生成/下載 NFT")
+if stock:
+    st.write("股票代碼:", stock)
